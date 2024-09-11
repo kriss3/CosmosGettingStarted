fff2cc4294c53c30908fa7218176c4d62a5ad116
Kris <kris_s2@outlook.com>
2024-09-10 23:32:37 -0700

    kata: preparing new proj to work with my sample cosmosDb ToDoList and Items collection;

---
+++ CosmosDb.App/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using static System.Console;
+
+namespace CosmosDb.App;
+
+public class Program
+{
+    static async Task Main()
+    {
+        WriteLine("Hello, World!");
+        await Task.CompletedTask;
+    }
+}

d8dd3d5c7d022e122d0199722935fcf0de2ea787
Kris <kris_s2@outlook.com>
2024-09-11 23:05:19 -0700

    kata: working on infra for existing ToDoItems Db in cosmos. Working on spliting caller from Db operations;

---
+++ CosmosDb.App/Model/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace CosmosDb.App.Model;
+public class Item
+{
+    public string Id { get; set; } = new Guid().ToString();
+    public string? Category { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+}
